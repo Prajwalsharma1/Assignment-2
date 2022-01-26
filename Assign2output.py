73c7f3e9e77b81d909e5e7013c11eb5e05a194bc
Prajwalsharma1 <97334755+Prajwalsharma1@users.noreply.github.com>
2022-01-26 08:31:33 -0800

    Add files via upload

---
+++ Assign2output.py
@@ -0,0 +1,45 @@
+Microsoft Windows [Version 10.0.19044.1415]
+(c) Microsoft Corporation. All rights reserved.
+
+C:\Users\prajw>cd desktop
+
+C:\Users\prajw\Desktop>Assign2input.py
+Question.1
+(a) part
+length of string is: 35
+(b) part
+egaugnal evitisnes esac a si nohtyP
+(c) part
+a case sensitive
+(d) part
+Python is a object oriented language
+(e) part
+10
+(f) part
+Pythonisacasesensitivelanguage
+Question.2
+Hey,Prajwal sharma is Here!
+My SID is 21103020
+I am from CSE department and my CGPA is 9.0
+Question.3
+(a) part
+8
+(b) part
+58
+(c) part
+50
+(d) part
+(e) part
+Question.4
+Please enter your number: 34
+Please enter your number: 65
+Please enter your number: 43
+Greatest number is :  65.0
+Question.5
+enter someting : prajwal
+No
+Question.6
+Enter first side length: 21
+Enter second side length: 36
+Enter third side length: 40
+YES,triangle can be formed
